a007926ddeff9997cee153ac13392684fb54357e
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-18 13:25:36 +0700

    Add files via upload

--- RANDOM_NUMBER_BASIC.py
+++ RANDOM_NUMBER_BASIC.py
@@ -1,4 +1,18 @@
 import random
 
+angka = input("masukkan angka: ")
+
+if angka.isdigit():
+    angka = int(angka)
+    if angka > 12:
+        print("masukkan angka yang lebih kecil")
+    elif angka < 1:
+        print("masukkan angka yang lebih besar")
+
+else:
+    print("input salah")
+    quit()
+
 random_angka = random.randint(1, 12)
 print(random_angka)
+
