505be0c012e41bd06a0f0f5a83590d7e21633669
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-23 19:07:38 +0700

    Add files via upload

---
+++ SIMPLE CALCULATOR.py
@@ -0,0 +1,22 @@
+entry1 = float(input("masukkan angka ke-1: "))
+operator = input("masukkan operator: ")
+entry2 = float(input("masukkan angka ke-2: "))
+
+
+if operator == "+":
+    print( entry1 + entry2)
+elif operator == "-":
+    print( entry1 - entry2)
+elif operator == "*":
+    print(entry1 * entry2)
+elif operator == "/":
+    print(entry1 / entry2)
+else:
+    print("operator salah")
+
+
+
+
+
+
+
